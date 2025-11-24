d3391911a53efe3bf0805c8c1354ae7df960f353
Marcio Battistini <m4.agents.ia@gmail.com>
2025-11-24 10:09:09 -0300

    ui: aproximar ainda mais os dois checks

--- src/components/WhatsAppChat/ChatArea/ChatArea.tsx
+++ src/components/WhatsAppChat/ChatArea/ChatArea.tsx
@@ -588,7 +588,7 @@ const MessageBubble: React.FC<MessageBubbleProps> = ({
             <svg className="w-4 h-4 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
             </svg>
-            <svg className="w-4 h-4 text-gray-400 -ml-2" fill="currentColor" viewBox="0 0 20 20">
+            <svg className="w-4 h-4 text-gray-400 -ml-3" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
             </svg>
           </div>
@@ -599,7 +599,7 @@ const MessageBubble: React.FC<MessageBubbleProps> = ({
             <svg className="w-4 h-4 text-blue-500" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
             </svg>
-            <svg className="w-4 h-4 text-blue-500 -ml-2" fill="currentColor" viewBox="0 0 20 20">
+            <svg className="w-4 h-4 text-blue-500 -ml-3" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
             </svg>
           </div>
@@ -610,7 +610,7 @@ const MessageBubble: React.FC<MessageBubbleProps> = ({
             <svg className="w-4 h-4 text-green-500" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
             </svg>
-            <svg className="w-4 h-4 text-green-500 -ml-2" fill="currentColor" viewBox="0 0 20 20">
+            <svg className="w-4 h-4 text-green-500 -ml-3" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
             </svg>
           </div>
